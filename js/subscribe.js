b26ef2185d210aba37cfab50e4589a71ae00ec47
Jonata Santos <118224160+jonatavsantos@users.noreply.github.com>
2024-12-03 15:37:44 +0000

    Inserindo opção de limitação de jogadores por modalidade no formulário

--- js/subscribe.js
+++ js/subscribe.js
@@ -1,16 +1,40 @@
-const playersContainer = document.getElementById('players-container');
-    const addPlayerButton = document.getElementById('add-player-button');
+let maxPlayers = 0;
 
-    addPlayerButton.addEventListener('click', () => {
-      const playerField = document.createElement('div');
-      playerField.classList.add('player-field');
-      playerField.innerHTML = `
-        <input type="text" name="player[]" placeholder="Digite o nome do jogador" required>
-        <button id="removebutton" type="button" onclick="removePlayer(this)">Remover</button>
-      `;
-      playersContainer.appendChild(playerField);
-    });
+        function atualizarCampos() {
+          const modalidade = document.getElementById("modalidade").value;
+          if (modalidade === "voleibol") {
+            maxPlayers = 9;  
+          } else if (modalidade === "futsal") {
+            maxPlayers = 10;  
+          } else {
+            maxPlayers = 0;
+          }
 
-    function removePlayer(button) {
-      button.parentElement.remove();
-    }
+          const container = document.getElementById("players-container");
+          container.innerHTML = '<div class="player-field"><input type="text" name="player[]" placeholder="Digite o nome do jogador" required><button type="button" class="remove-player-button" onclick="removerJogador(this)">Remover</button></div>';
+        }
+
+        function adicionarJogador() {
+          const modalidade = document.getElementById("modalidade").value;
+          if (!modalidade) {
+            alert("Por favor, selecione sua modalidade antes de adicionar jogadores.");
+            return;
+          }
+
+          const container = document.getElementById("players-container");
+          const currentPlayers = container.getElementsByClassName("player-field").length;
+          
+          if (currentPlayers < maxPlayers) {
+            const novoCampo = document.createElement("div");
+            novoCampo.classList.add("player-field");
+            novoCampo.innerHTML = `<input type="text" name="player[]" placeholder="Digite o nome do jogador" required><button type="button" class="remove-player-button" onclick="removerJogador(this)">Remover</button>`;
+            container.appendChild(novoCampo);
+          } else {
+            alert("Número máximo de jogadores atingido.");
+          }
+        }
+        
+        function removerJogador(button) {
+          const campoJogador = button.parentElement;
+          campoJogador.remove();
+        }
