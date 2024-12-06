89e2e9b17f658a9fdd140893bb24f06ef3b0e391
jonatavsantos <surinegann@gmail.com>
2024-12-06 00:26:28 -0300

    Intercurso 2.0, foi atualizado a forma com que se inscreve no torneio, agora há páginas exclusivas para se inscrever em cada modalidade, ainda não foram concluidas todas as atualizações necessárias para utilização

--- js/subscribe.js
+++ js/subscribe.js
@@ -1,10 +1,9 @@
-let maxPlayers = 0;
         function adicionarGenero() {
 
           const genero = document.getElementById("gender").value;
 
           if (!genero) {
-            alert("Por favor, selecione o gênero da equipe antes de adicionar jogadores!")
+            alert("Por favor, selecione o gênero da equipe antes de enviar o formulário!")
             return false;
           }
           return true;
@@ -14,69 +13,33 @@ let maxPlayers = 0;
           const curso = document.getElementById("course").value;
 
           if (!curso) {
-            alert("Por favor, selecione o curso antes de adicionar jogadores!")
+            alert("Por favor, selecione o curso antes de enviar o formulário!")
             return false;
           }
           return true;
         }
 
-        function atualizarCampos() {
-          const modalidade = document.getElementById("modalidade").value;
-          if (modalidade === "voleibol") {
-            maxPlayers = 11;  
-          } else if (modalidade === "futsal") {
-            maxPlayers = 11;  
-          } else if (modalidade === "basquete") {
-            maxPlayers = 11;
-          } else if (modalidade === "handebol") {
-            maxPlayers = 11;
-          } else if (modalidade === "xadrez") {
-            maxPlayers = 1;
-          } else if (modalidade === "tenismesa") {
-            maxPlayers = 1;
-          } else if (modalidade === "praia") {
-            maxPlayers = 1;
-          } else {
-            maxPlayers = 0;
+        function validarTelefone() {
+          const telefone = document.getElementById("captain-phone").value;
+          const padraoBrasileiro = /^(\(?\d{2}\)?\s?)?\d{5}-?\d{4}$/;
+        
+          if (!padraoBrasileiro.test(telefone)) {
+            alert("Por favor, insira um número de telefone válido no formato (XX) XXXXX-XXXX.");
+            return false;
           }
-
-          const container = document.getElementById("players-container");
-          container.innerHTML = '<div class="player-field"><input type="text" name="player[]" placeholder="Digite o nome do jogador" required><button type="button" class="remove-player-button" onclick="removerJogador(this)">Remover</button></div>';
-        }
-
-        function adicionarJogador() {
-
-          const validarGen = adicionarGenero()
-          if (!validarGen) {
-            return;
-          }
-
-          const validarCur = adicionarCurso()
-          if (!validarCur) {
-            return;
-          }
-
-          const modalidade = document.getElementById("modalidade").value;
-
-          if (!modalidade) {
-            alert("Por favor, selecione sua modalidade antes de adicionar jogadores!");
-            return;
-          }
-
-          const container = document.getElementById("players-container");
-          const currentPlayers = container.getElementsByClassName("player-field").length;
-          
-          if (currentPlayers < maxPlayers) {
-            const novoCampo = document.createElement("div");
-            novoCampo.classList.add("player-field");
-            novoCampo.innerHTML = `<input type="text" name="player[]" placeholder="Digite o nome do jogador" required><button type="button" class="remove-player-button" onclick="removerJogador(this)">Remover</button>`;
-            container.appendChild(novoCampo);
-          } else {
-            alert("Número máximo de jogadores atingido.");
-          }
+        
+          return true;
         }
         
-        function removerJogador(button) {
-          const campoJogador = button.parentElement;
-          campoJogador.remove();
-        }
+
+        function validarForm(event) {
+        
+          if (!adicionarGenero() && !adicionarCurso() && !validarTelefone) {
+            event.preventDefault(); 
+            return false;
+          };
+          
+          alert("Sua inscrição foi recebida, QUE COMECEM OS JOGOS!");
+        };
+      
+        document.getElementById("team-form").addEventListener("submit", validarForm);
